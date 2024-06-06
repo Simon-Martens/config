fa69e9eedb0e95fc9e83b5f5486907cf73ed13fc
Simon Martens <simonmartens@mailbox.org>
2024-06-06 22:51:47 +0200

    Added tailwind-fold plugin, alacritty config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -138,6 +138,8 @@ vim.keymap.set('n', '<leader>gm', vim.diagnostic.goto_next, { desc = 'Go to next
 vim.keymap.set('n', '<leader>m', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, { noremap = true, silent = true })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
