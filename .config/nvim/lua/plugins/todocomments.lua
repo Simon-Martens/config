1e194e6f1c09e1778eee18c2d833cb541da0447e
Simon Martens <simon.martens@mailbox.org>
2024-12-30 09:27:03 +0100

    Some ghostty settings

--- .config/nvim/lua/plugins/todocomments.lua
+++ .config/nvim/lua/plugins/todocomments.lua
@@ -4,4 +4,9 @@ return {
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = { signs = false },
+  config = function()
+    require('todo-comments').setup()
+    vim.keymap.set('n', '<leader>st', '<cmd>TodoTelescope keywords=TODO<cr>')
+    vim.keymap.set('n', '<leader>sb', '<cmd>TodoTelescope keywords=PERF,BUG,FIX<cr>')
+  end,
 }
