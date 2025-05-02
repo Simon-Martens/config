0a9e31949ae46ab0980ebb1e9bc1f05e1d700d8d
Simon Martens <simon.martens@mailbox.org>
2025-05-02 23:49:10 +0200

    VSCODE :(

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 sudo pacman -Syu firefox gnome-extra gnome-tweaks gnome-control-center gdm gnome-shell gnome-backgrounds gnome-settings-daemon gnome-session gnome-terminal gnome-system-monitor gnome-disk-utility gnome-calculator gnome-characters gnome-contacts gnome-font-viewer gnome-logs gnome-menus gnome-screenshot gnome-shell-extensions gnome-themes-extra gvfs gvfs-mtp gvfs-nfs gvfs-smb nautilus networkmanager xdg-user-dirs xdg-utils gnome-power-manager git zoxide bluez bluez-utils pipewire pipewire-pulse pipewire-alsa wireplumber python-pipx wl-clipboard ffmpeg gstreamer gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav gstreamer-vaapi base-devel cmake ninja curl tmux jq gtk4 libadwaita blueprint-compiler gettext power-profiles-daemon hyprland brightnessctl fzf wofi ninja gcc wayland-protocols libjpeg-turbo libwebp libjxl pango cairo pkgconf cmake libglvnd wayland hyprutils hyprwayland-scanner hyprlang hyprpaper xdg-desktop-portal-hyprland qt5-wayland qt6-wayland blueman waybar fzf brightnessctl keepassxc ntfs-3g exfatprogs bat
+
 gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
 gsettings set org.gnome.shell disable-user-extensions false
+
 sudo systemctl enable --now bluetooth.service
-systemctl --user enable --now pipewire pipewire-pulse wireplumber
+sudo systemctl --user enable --now pipewire pipewire-pulse wireplumber
+sudo systemctl enable --now power-profiles-daemon
 
 mkdir ~/source
 cd ~/source
@@ -12,4 +15,9 @@ git clone https://aur.archlinux.org/widevine.git
 cd widevine
 makepkg -si
 
-sudo systemctl enable --now power-profiles-daemon
+cd ~/source
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -S visual-studio-code-bin
--- scripts/bashrc/nvim.sh
+++ scripts/bashrc/nvim.sh
@@ -4,7 +4,6 @@ alias e='nvim'
 alias vim='nvim'
 alias v='nvim'
 
-
 export MANPAGER='nvim +Man!'
 export EDITOR="nvim"
 export VISUAL="nvim"
