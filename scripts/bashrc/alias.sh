2fa350cfe4c54164ea716360c2d5159a970bfc79
Simon Martens <simon.martens@mailbox.org>
2025-05-20 16:09:05 +0200

    some changes

--- scripts/bashrc/alias.sh
+++ scripts/bashrc/alias.sh
@@ -11,3 +11,18 @@ alias i='sudo pacman -Syu'
 # removal protection
 alias rm='rm -I'
 
+# dstask aliases
+alias d='dstask'
+alias da='dstask add'
+alias ds='dstask start'
+alias dd='dstask done'
+alias dc='dstask context'
+alias dm='dstask modify'
+alias dl='dstask log'
+alias de='dstask edit'
+alias dn='dstask note'
+alias dsync='dstask sync'
+alias dnote='dstask note'
+alias dstop='dstask stop'
+alias drm='dstask remove'
+
