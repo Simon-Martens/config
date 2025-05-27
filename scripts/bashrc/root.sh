40dc4fbf34780af786b4871f40d8ea1d5b19c3f3
Simon Martens <simon.martens@mailbox.org>
2025-05-27 20:37:41 +0200

    new bashrc

---
+++ scripts/bashrc/root.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export BASH_ENV="~/.bash_env"
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+    . /etc/bashrc
+fi
+
+if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+fi
