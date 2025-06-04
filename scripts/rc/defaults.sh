3b51418b445fa3029ef79921141fb323e8934b7b
Simon Martens <simon.martens@mailbox.org>
2025-06-04 14:35:44 +0200

    Mixed

--- scripts/rc/defaults.sh
+++ scripts/rc/defaults.sh
@@ -1,5 +1,4 @@
 export TERMINAL="ghostty"
 export MANPAGER='nvim +Man!'
-export PAGER='nvim +%s'
 export EDITOR="nvim"
 export VISUAL="nvim"
--- scripts/rc/path.sh
+++ scripts/rc/path.sh
@@ -1,7 +1,32 @@
-if [ -d "$HOME/bin" ]; then
-	PATH="$HOME/bin:$PATH"
-	export PATH
+# GO
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$HOME/go/bin
+
+# Zig
+export PATH=$PATH:/usr/local/zig
+
+# Bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# Ruby
+export GEM_HOME="$HOME/gems"
+export PATH="$HOME/gems/bin:$PATH"
+
+# Dotnet
+export PATH=$PATH:/home/simon/.dotnet
+
+# NVM
+if [ -d "$HOME/.nvm" ]; then
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 fi
 
+# Rust
+if [ -f "$HOME/.cargo/env" ]; then
+    . "$HOME/.cargo/env"
+fi
 
-# export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
+# Additional and custom executable paths
+PATH=$HOME/.local/bin:$HOME/bin:$PATH
